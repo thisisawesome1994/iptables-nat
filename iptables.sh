79b861bd8b16e802a11ad4ceb1677e1902706958
thisisawesome1994 <58063460+thisisawesome1994@users.noreply.github.com>
2022-05-05 04:13:33 +0200

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -3,5 +3,8 @@ iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -i eno1 -j ACCEPT
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -t nat -A POSTROUTING -o ens4 -j MASQUERADE
+ip6tables -t nat -A POSTROUTING -o ens4 -j MASQUERADE
 iptables -A FORWARD -i ens4 -o eno1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+ip6tables -A FORWARD -i ens4 -o eno1 -m state –state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i eno1 -o ens4 -j ACCEPT
+ip6tables -A FORWARD -i eno1 -o ens4 -j ACCEPT
